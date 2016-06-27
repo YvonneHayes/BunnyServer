59024af74c5dd83fa1dc495aa3eb80a5d6067c9a
David <goodwid@gmail.com>
2016-06-27 15:13:48 -0700

    Added mongoose backend for bunnies

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const app = module.exports = express();
+const Bunny = require('./lib/bunnies');
+const bodyParser = require('body-parser').json();
 
 app.use(express.static(__dirname+'/public'));
+
+app.get('/api/bunny', (req, res) => {
+  Bunny.find()
+    .lean()
+    .then(results => res.json(results))
+    .catch(err => {
+      console.log('got here');
+      res.status(500).json({
+        msg: 'unable to find bunnies',
+        error: err
+      });
+    });
+});
+
+app.post('/api/bunny', bodyParser, (req, res) => {
+  new Bunny(req.body).save()
+    .then(result => res.send(result))
+    .catch(err => {
+      console.log(err);
+      res.json({err});
+    });
+});
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const app = require('./app');
 const port = process.argv[2] || 9000;
+require ('./lib/setup-mongoose');
 
 app.listen(port, err =>{
   if (err) return console.error(err);
