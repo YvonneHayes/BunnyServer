b363e8e012d12ea6ab5a47425ae1a709846bf2b3
David <goodwid@gmail.com>
2016-06-28 15:52:51 -0700

    Added cors support

--- routes/albums.js
+++ routes/albums.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Album = require('../models/album');
+const Image = require('../models/image');
 const router = express.Router();
 const bodyParser = require('body-parser').json();
 
@@ -9,12 +10,12 @@ router
       .lean()
       .then(results => res.json(results))
       .catch(err => res.status(500).json({error: {message: 'Unable to find records',reason: err}}));
-
   })
-  .post('/', bodyParser, (req, res) => {
-    new Album(req.body).save()
-      .then(result => res.send(result))
-      .catch(err => res.status(500).json({error: {message: 'Unable to find record', reason: err}}));
+  // returns all images associated with the specified album
+  .get('/:id', bodyParser, (req, res) => {
+    Image.find({album: req.params.id})
+      .then(results => res.json(results))
+      .catch(err => res.status(500).json({error: {message: 'Unable to find records', reason: err}}));
   })
   .post('/', bodyParser, (req, res) => {
     new Album(req.body)
